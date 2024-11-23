e024303e80cdabbaf5b962c840ca9ed794fbb39c
virendra-978 <virendrat2121@gmail.com>
2024-11-23 18:09:25 +0530

    Fourth Commit

--- Prime.java
+++ Prime.java
@@ -2,6 +2,10 @@ class Main {
     public static void main(String[] args) {
         int n = 13;
         int count = 0;
-
+        for(int i=1; i<=n; i++){
+            if(n % i == 0)
+             count++;
+        }
+        
     }
 }
